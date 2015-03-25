f2c3b5bb39ab3e60abb6e8fd7a9005c8cbff9050
Vlatko Ivanovski <vlatko@mkdot.net>
2015-03-25 12:07:57 +0100

    Init project

---
+++ GitHubWebApp/GitHubWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitHubWebApp.Startup))]
+namespace GitHubWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
